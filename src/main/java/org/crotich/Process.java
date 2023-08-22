512df8e19d7c779e951ae0d88343f785f9f56f32
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-08-22 10:36:57 +0300

    Implemented modulo operator for keys > 26

--- src/main/java/org/crotich/Process.java
+++ src/main/java/org/crotich/Process.java
@@ -7,15 +7,24 @@ public class Process {
         String output = "";
 
         for(char c: chars){
-            int pos = ALPHABETS.indexOf(c);
-            int newPos = pos + key;
+            int pos = ALPHABETS.indexOf(Character.toUpperCase(c));
+            int newPos = (pos + key) % 26;
             char replacement = ALPHABETS.charAt(newPos);
             output = output + replacement;
         }
         return output;
     }
     public static String decode(String message, int key){
-        return "decode";
+        char[] chars = message.toCharArray();
+        String output = "";
+
+        for(char c: chars){
+            int pos = ALPHABETS.indexOf(c);
+            int newPos = pos - key;
+            char replacement = ALPHABETS.charAt(newPos);
+            output = output + replacement;
+        }
+        return output;
     }
     public static String run(Data data){
         if(data.getProcess() == 1){
