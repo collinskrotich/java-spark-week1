838a640cc1a93c11d898a393a9cf822c43d2a900
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-08-22 08:18:47 +0300

    Added encode and decode to Process.java

--- src/main/java/org/crotich/Process.java
+++ src/main/java/org/crotich/Process.java
@@ -1,4 +1,20 @@
 package org.crotich;
 
 public class Process {
+    public static encode(String message, int key){
+        return "";
+    }
+    public static decode(String message, int key){
+        return "";
+    }
+    public String run(Data data){
+        if(data.getProcess() == 1){
+            return encode(data.getMessage(), data.getKey());
+        }else if (data.getProcess() == 2){
+            return decode(data.getMessage(), data.getKey());
+        }else {
+            return "";
+        }
+
+    }
 }
