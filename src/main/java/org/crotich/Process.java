a2b9562c2fabd188c124877e7c9a070c6963623c
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-08-22 08:53:58 +0300

    Implemented the setters to main.java

--- src/main/java/org/crotich/Process.java
+++ src/main/java/org/crotich/Process.java
@@ -1,13 +1,13 @@
 package org.crotich;
 
 public class Process {
-    public static encode(String message, int key){
-        return "";
+    public static String encode(String message, int key){
+        return "encode";
     }
-    public static decode(String message, int key){
-        return "";
+    public static String decode(String message, int key){
+        return "decode";
     }
-    public String run(Data data){
+    public static String run(Data data){
         if(data.getProcess() == 1){
             return encode(data.getMessage(), data.getKey());
         }else if (data.getProcess() == 2){
